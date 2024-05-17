a3d3b0644f45efd05d798695eedae0e21e7f87f2
Sabersand <josemgsanchez4@gmail.com>
2024-05-17 18:04:03 -0400

    Inicio

--- src/ApiConversor.java
+++ src/ApiConversor.java
@@ -1,3 +1,10 @@
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+
 public class ApiConversor {
-    
+    HttpClient client = HttpClient.newHttpClient();
+    HttpRequest request = HttpRequest.newBuilder()
+            .uri(URI.create("https://v6.exchangerate-api.com/v6/70171c9556e4044c55ac3108/latest/USD"))
+            .build();
 }
