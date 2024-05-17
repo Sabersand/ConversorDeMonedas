3200f048e12eea611d9d6031d6eeb24d66eb2168
Sabersand <josemgsanchez4@gmail.com>
2024-05-17 18:13:00 -0400

    Inicio

--- src/ApiConversor.java
+++ src/ApiConversor.java
@@ -1,10 +1,26 @@
+import java.io.IOException;
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
 
 public class ApiConversor {
+
+
     HttpClient client = HttpClient.newHttpClient();
     HttpRequest request = HttpRequest.newBuilder()
             .uri(URI.create("https://v6.exchangerate-api.com/v6/70171c9556e4044c55ac3108/latest/USD"))
             .build();
+
+    HttpResponse<String> response;
+    {
+        try {
+            response = client
+                    .send(request, HttpResponse.BodyHandlers.ofString());
+        } catch (IOException | InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+
 }
