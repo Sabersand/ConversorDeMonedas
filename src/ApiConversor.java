5b19748dd3c67cff3ced64ba7b4e30dc96da4d6b
José González Sánchez <158603167+Sabersand@users.noreply.github.com>
2024-05-21 10:05:37 -0400

    Update ApiConversor.java

--- src/ApiConversor.java
+++ src/ApiConversor.java
@@ -6,26 +6,19 @@ import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 
-public class ApiConversor {
+public Moneda obtenerTasaDeCambio(String divisaBase, String divisaObjetivo) {
+    HttpClient client = HttpClient.newHttpClient();
+    URI uriURL = URI.create("https://v6.exchangerate-api.com/v6/70171c9556e4044c55ac3108/pair/" + divisaBase + "/" + divisaObjetivo);
 
-    public Moneda moneda(String divisa, String cambio, double valor) {
-        HttpClient client = HttpClient.newHttpClient();
+    HttpRequest request = HttpRequest.newBuilder().uri(uriURL).build();
 
-        URI uriURL = URI.create("https://v6.exchangerate-api.com/v6/70171c9556e4044c55ac3108/latest/"+divisa+"/"+cambio+"/"+valor);
-        HttpRequest request = HttpRequest.newBuilder()
-                .uri(uriURL)
-                .build();
-
-        HttpResponse<String> response;
-        {
-            try {
-                response = client
-                        .send(request, HttpResponse.BodyHandlers.ofString());
-            } catch (Exception e) {
-                throw new RuntimeException("No fue posible procesar tu solicitud");
-            }
-            return new Gson().fromJson(response.body(), Moneda.class);
+    try {
+        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+        if (response.statusCode() != 200) {
+            throw new RuntimeException("Error en la solicitud: " + response.statusCode());
         }
-
+        return new Gson().fromJson(response.body(), Moneda.class);
+    } catch (IOException | InterruptedException e) {
+        throw new RuntimeException("No fue posible procesar tu solicitud", e);
     }
 }
